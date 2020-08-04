9b9832ba6e26a6b4f1ccb3d5e86f0c0841db4cbf
logan <logan.testa@outlook.com>
2020-08-03 17:09:23 -0700

    Limit access to JSON data

--- public/blog/wp-content/themes/yamhillfieldsnursery/functions.php
+++ public/blog/wp-content/themes/yamhillfieldsnursery/functions.php
@@ -1,4 +1,17 @@
 <?php
+
+/*Prevent non-logged in users from accessing the site's user and post data using the WordPress REST API.*/
+add_filter( 'rest_authentication_errors', function( $result ) {
+   if ( !empty( $result ) ) {
+       return $result;
+   } 
+   if( !is_user_logged_in() ) {
+       return new WP_Error( 'rest_not_logged_in', 'You are not currently logged in.', array( 'status' => 401 ) );    
+   }
+   return $result;
+});
+
+
 add_theme_support( "post-thumbnails" ); //Allow image thumbnails in pages and posts.
 //Allow cropping for medium thumbnail images.
 if(false === get_option( "medium_crop" )) {
