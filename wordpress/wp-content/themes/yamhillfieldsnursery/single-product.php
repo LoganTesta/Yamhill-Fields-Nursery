e2f442b3328739dc48248d9e452863eab1ab64a8
logan <logan.testa@outlook.com>
2020-11-16 12:35:49 -0800

    Adust layout for single-product template

--- wordpress/wp-content/themes/yamhillfieldsnursery/single-product.php
+++ wordpress/wp-content/themes/yamhillfieldsnursery/single-product.php
@@ -7,16 +7,11 @@ get_header();
 <div class="inner-wrapper">
     <div class="content page-content">
         <div class="content-row">
-            <div class="col-sma-5">
+            <div class="col-sma-12">
                 <?php while( have_posts() ) : 
                 the_post();
                 wc_get_template_part( 'content', 'single-product' );
                 endwhile ?>
-            </div>
-            <div class="col-sma-7">
-                <div class="content-background-container">
-                    <div class="content__content-image <?php if ( esc_url( trim( the_post_thumbnail_url() ) ) === "" ) { echo "hide"; } ?>" style="background: url('<?php echo esc_url( the_post_thumbnail_url() ); ?>') 50% 50%/cover no-repeat;"></div>    
-                </div>
             </div>
         </div>
     </div>
