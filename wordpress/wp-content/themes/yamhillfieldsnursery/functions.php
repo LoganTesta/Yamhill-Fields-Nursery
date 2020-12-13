7f213226f6b0976c8f0371b52e37535b87a7848a
logan <logan.testa@outlook.com>
2020-12-12 20:03:19 -0800

    Adjust functions logic enabled

--- wordpress/wp-content/themes/yamhillfieldsnursery/functions.php
+++ wordpress/wp-content/themes/yamhillfieldsnursery/functions.php
@@ -1,15 +1,15 @@
 <?php
 
 /*Prevent non-logged in users from accessing the site's user and post data using the WordPress REST API.*/
-add_filter( 'rest_authentication_errors', function( $result ) {
-   if ( !empty( $result ) ) {
-       return $result;
-   } 
-   if( !is_user_logged_in() ) {
-       return new WP_Error( 'rest_not_logged_in', 'You are not currently logged in.', array( 'status' => 401 ) );    
-   }
-   return $result;
-});
+//add_filter( 'rest_authentication_errors', function( $result ) {
+//   if ( !empty( $result ) ) {
+//       return $result;
+//   } 
+//   if( !is_user_logged_in() ) {
+//       return new WP_Error( 'rest_not_logged_in', 'You are not currently logged in.', array( 'status' => 401 ) );    
+//   }
+//   return $result;
+//});
 
 
 
