355e72667e456872d77ad0b0927ed154fe07dd12
logan <logan.testa@outlook.com>
2021-09-15 19:56:08 -0700

    Update General Testimonials

--- wordpress/wp-content/plugins/general-testimonials/assets/css/general-testimonials-styles.php
+++ wordpress/wp-content/plugins/general-testimonials/assets/css/general-testimonials-styles.php
@@ -8,6 +8,7 @@ include ( plugin_dir_path(__FILE__) . "/general-testimonials.php" );
 
 
 $numberOfTestimonialsPerRow = (int)( get_option( 'general-testimonials-testimonials-per-row' ) );
+$numberOfTestimonialsPerRowTablet = (int)( get_option( 'general-testimonials-testimonials-per-row' ) );
 $testimonialWidthTablet = 50;
 $testimonialWidth;
 if ( $numberOfTestimonialsPerRow <= 0 ) {
@@ -15,6 +16,9 @@ if ( $numberOfTestimonialsPerRow <= 0 ) {
 }
 if ( $numberOfTestimonialsPerRow < 2) {
     $testimonialWidthTablet = 100;
+}
+if ( $numberOfTestimonialsPerRow > 2 ) {
+    $numberOfTestimonialsPerRowTablet = 2;
 }
 $testimonialWidth = 100/$numberOfTestimonialsPerRow;
 
@@ -64,19 +68,28 @@ if ( $generalTestimonialsFloatImageDirection === "left" ) {
 
 
 @media only screen and (min-width: 700px){
-     /* Clearing variable width columns */
-    .testimonials-container__inner-wrapper .testimonial:nth-child(<?php echo $numberOfTestimonialsPerRow; ?>n+1){ content: ""; display: block; clear: both; }  
+    /* Clearing variable width columns */
   
     .testimonial { float: left; width: <?php echo $testimonialWidthTablet; ?>%; padding: 0 20px 50px 20px; }
     
     .testimonial__image { float: <?php echo $generalTestimonialsFloatImageDirection; ?>; margin-bottom: 15px; margin-left: <?php echo $generalTestimonialsImageTabletPlusMarginLeft; ?>; margin-right: <?php echo $generalTestimonialsImageTabletPlusMarginRight; ?>; }
-    
-    .testimonial:nth-of-type(<?php echo $numberOfTestimonialsPerRow; ?>n+1) { padding-left: 0; }
-    .testimonial:nth-of-type(<?php echo $numberOfTestimonialsPerRow; ?>n+<?php echo $numberOfTestimonialsPerRow; ?>) { padding-right: 0; }
+}
+
+
+
+@media only screen and (min-width: 700px) and (max-width: 1199px){
+   /* Clearing variable width columns */
+    .testimonials-container__inner-wrapper .testimonial:nth-child(<?php echo $numberOfTestimonialsPerRowTablet; ?>n+1){ content: ""; display: block; clear: both; }  
+
+    .testimonial:nth-of-type(<?php echo $numberOfTestimonialsPerRowTablet; ?>n+1) { padding-left: 0; }
+    .testimonial:nth-of-type(<?php echo $numberOfTestimonialsPerRowTablet; ?>n+<?php echo $numberOfTestimonialsPerRowTablet; ?>) { padding-right: 0; } 
 }
 
 
 
 @media only screen and (min-width: 1200px){ 
     .testimonial { width: <?php echo $testimonialWidth; ?>%; }
+    
+    .testimonial:nth-of-type(<?php echo $numberOfTestimonialsPerRow; ?>n+1) { padding-left: 0; }
+    .testimonial:nth-of-type(<?php echo $numberOfTestimonialsPerRow; ?>n+<?php echo $numberOfTestimonialsPerRow; ?>) { padding-right: 0; }
 }
