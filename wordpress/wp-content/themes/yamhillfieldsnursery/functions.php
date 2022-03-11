9700d4d2db5910ba500864aacaeb3a84a1cdc926
logan <logan.testa@outlook.com>
2022-03-10 22:58:12 -0800

    Change border styling for inputs

--- wordpress/wp-content/themes/yamhillfieldsnursery/functions.php
+++ wordpress/wp-content/themes/yamhillfieldsnursery/functions.php
@@ -69,7 +69,7 @@ add_action( 'wp_enqueue_scripts', function() {
  
     wp_register_script( 'javascript-functions', get_template_directory_uri() . '/assets/javascript/javascript-functions.js' );
     wp_enqueue_script( 'javascript-functions', get_template_directory_uri() . '/assets/javascript/javascript-functions.js' );  
-    wp_enqueue_style( 'styles', "" . get_template_directory_uri() . '/assets/css/main-styles.css?mod=03092022' );
+    wp_enqueue_style( 'styles', "" . get_template_directory_uri() . '/assets/css/main-styles.css?mod=03102022' );
     wp_enqueue_style( 'print-styles', "" . get_template_directory_uri() . '/assets/css/print-styles.css?mod=11092020' );
    
     wp_register_script( 'item-hover-over-zoom-in', get_template_directory_uri() . '/assets/javascript/item-hover-over-zoom-in.js' );
