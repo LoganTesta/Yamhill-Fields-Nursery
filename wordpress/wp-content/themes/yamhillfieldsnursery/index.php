df5d374c52e6aeb54b41ce3d67e337430fb3fd5e
logan <logan.testa@outlook.com>
2022-10-12 21:13:23 -0700

    Improve logic for page content images

--- wordpress/wp-content/themes/yamhillfieldsnursery/index.php
+++ wordpress/wp-content/themes/yamhillfieldsnursery/index.php
@@ -14,7 +14,7 @@ get_header();
                     <?php while ( have_posts() ) : the_post(); //A while loop is necessary to call the_content(). ?>
                         <div class="col-sma-12">
                             <div class="content-background-container general-layout">
-                                <div class="content__content-image <?php if ( esc_url( trim( the_post_thumbnail_url() ) ) === "" ) { echo "hide"; } ?>" style="background: url('<?php echo esc_url( the_post_thumbnail_url() ); ?>') 50% 50%/cover no-repeat;"></div>    
+                                <div class="content__content-image <?php if ( esc_url( trim( get_the_post_thumbnail_url() ) ) === "" ) { echo "hide"; } ?>" style="background: url('<?php echo esc_url( get_the_post_thumbnail_url() ); ?>') 50% 50%/cover no-repeat;"></div>    
                             </div>
                             <h2><?php single_post_title(); ?></h2>
                             <?php the_content(); ?>
