5725d3d9b00b3005527902e558572abc6420c3cd
logan <logan.testa@outlook.com>
2023-03-22 21:45:52 -0700

    Add category page title, add post styling

--- wordpress/wp-content/themes/yamhillfieldsnursery/category.php
+++ wordpress/wp-content/themes/yamhillfieldsnursery/category.php
@@ -6,18 +6,22 @@ get_header();
 <div class="inner-wrapper">
     <div class="content page-content">
         <div class="content-row">
+            <div class="col-sma-12">
+            <?php if ( have_posts() ) { ?>
+                <h2 class="">Category: <?php echo single_cat_title(); ?></h2>
+            <?php } ?>
             <?php while ( have_posts() ) : the_post(); //A while loop is necessary to call the_content(). ?>
-                <div class="col-sma-12 category-section">
+                <div class="category-blog-post">
                     <div class="content-background-container general-layout">
                         <div class="content__content-image <?php if ( esc_url( trim( get_the_post_thumbnail_url() ) ) === "" ) { echo "hide"; } ?>" style="background: url('<?php echo esc_url( get_the_post_thumbnail_url() ); ?>') 50% 50%/cover no-repeat;"></div>    
                     </div>
-                    <h2><?php single_post_title(); ?></h2>
+                    <h3><?php the_title(); ?></h3>
                     <?php the_content(); ?>
                 </div>
             <?php 
             endwhile;
             wp_reset_query(); //Reset the page query ?>
-            
+            </div>
         </div>
     </div>
 </div>   
