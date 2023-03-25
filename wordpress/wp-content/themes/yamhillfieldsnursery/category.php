ab0afd6fc0eaa0fd0c5f0094b4e47ba38d8bdcc1
logan <logan.testa@outlook.com>
2023-03-24 21:39:34 -0700

    Rework layout of category page

--- wordpress/wp-content/themes/yamhillfieldsnursery/category.php
+++ wordpress/wp-content/themes/yamhillfieldsnursery/category.php
@@ -1,31 +1,44 @@
 <?php
 
+//The default page template and layout.
 get_header(); 
 ?>
 
+
 <div class="inner-wrapper">
     <div class="content page-content">
-        <div class="content-row">
-            <div class="col-sma-12">
-            <?php if ( have_posts() ) { ?>
-                <h2 class="">Category: <?php echo single_cat_title(); ?></h2>
-            <?php } ?>
-            <?php while ( have_posts() ) : the_post(); //A while loop is necessary to call the_content(). ?>
-                <div class="category-blog-post">
-                    <div class="content-background-container general-layout">
-                        <div class="content__content-image <?php if ( esc_url( trim( get_the_post_thumbnail_url() ) ) === "" ) { echo "hide"; } ?>" style="background: url('<?php echo esc_url( get_the_post_thumbnail_url() ); ?>') 50% 50%/cover no-repeat;"></div>    
-                    </div>
-                    <h3><?php the_title(); ?></h3>
-                    <?php the_content(); ?>
-                </div>
-            <?php 
-            endwhile;
-            wp_reset_query(); //Reset the page query ?>
+        <div class="content-row">     
+            <div class="col-lar-9 blog-col-left">
+                <h2>Category: <?php echo single_cat_title(); ?></h2>            
+                <div class="blog-posts" id="blogPosts">
+                    <?php
+                    global $post;
+                    $args = array( 'posts_per_page' => 1000 );
+                    $postsToDisplay = get_posts( $args );
+                    foreach ( $postsToDisplay as $post ) : setup_postdata( $post );
+                        ?>                                                       
+                        <div class="blog-post" id="<?php the_title(); ?>">
+                            <?php if ( has_post_thumbnail() ) { ?><div class="blog__image" style="background: url('<?php echo esc_url( the_post_thumbnail_url( 'medium' ) ); ?>') 50% 50%/cover no-repeat"></div><?php } ?>
+                            <div class="blog__content-wrapper">
+                                <h3 class="blog-post__title"><a class="blog-post__title__link" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                                <div class="blog__date"><?php the_date(); ?></div>
+                                <div class="blog__content"><?php the_excerpt(); ?></div>
+                            </div>
+                            <div class="clear-both"></div>
+                        </div>
+                    <?php endforeach; ?>
+                </div>               
+            </div>
+            <div class="col-lar-3 blog-col-right">
+                <h3>Visit us at</h3>
+                <p>45202 Oak Highway, Dundee, OR 97115</p>
+                <p>Open Mon-Sat 9:00 am-9:00 pm, closed Sunday.</p>
+                <div class="blog-col-right__bg-img"></div>
             </div>
         </div>
     </div>
-</div>   
-
+</div>                         
+        
 <?php 
 get_footer();
 ?>
