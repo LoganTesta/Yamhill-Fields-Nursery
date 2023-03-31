7abbc655af9d4d1283435aead0bf83b7eac8a4b9
logan <logan.testa@outlook.com>
2023-03-30 20:38:23 -0700

    Add on the categories pages each blog's categories

--- wordpress/wp-content/themes/yamhillfieldsnursery/category.php
+++ wordpress/wp-content/themes/yamhillfieldsnursery/category.php
@@ -32,6 +32,26 @@ get_header();
                             <?php if ( has_post_thumbnail() ) { ?><div class="blog__image" style="background: url('<?php echo esc_url( the_post_thumbnail_url( 'medium' ) ); ?>') 50% 50%/cover no-repeat"></div><?php } ?>
                             <div class="blog__content-wrapper">
                                 <h3 class="blog-post__title"><a class="blog-post__title__link" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                                    <div class="blog__categories"><?php
+                                        $categories = get_the_category();
+                                        $h = 0;
+                                        foreach ( $categories as $category ) {
+                                            $h++;
+                                        }
+                                        $h = $h - 1;
+                                        $i = 0;
+                                        foreach ( $categories as $category ) {
+                                            $result = "";
+                                            if ( $i < $h ) {
+                                                $result .= "<a class='blog__categories__link' href='" . get_category_link( $category ) . "'>" . $category->name . "</a>, ";
+                                            } else {
+                                                $result .= "<a class='blog__categories__link' href='" . get_category_link( $category ) . "'>" . $category->name . "</a>";
+                                            }
+                                            echo $result;
+                                            $i++;
+                                        }
+                                        ?>
+                                    </div>
                                 <div class="blog__date"><?php the_date(); ?></div>
                                 <div class="blog__content"><?php the_excerpt(); ?></div>
                             </div>
