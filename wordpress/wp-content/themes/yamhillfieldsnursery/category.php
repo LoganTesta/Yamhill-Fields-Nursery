6d4db087f7352e99484cf90feb67b71779099ee4
logan <logan.testa@outlook.com>
2023-04-07 21:43:03 -0700

    Make blog post images links on the main blog and category pages

--- wordpress/wp-content/themes/yamhillfieldsnursery/category.php
+++ wordpress/wp-content/themes/yamhillfieldsnursery/category.php
@@ -30,7 +30,9 @@ get_header();
                     foreach ( $postsToDisplay as $post ) : setup_postdata( $post );
                         ?>                                                       
                         <div class="blog-post" id="<?php the_title(); ?>">
-                            <?php if ( has_post_thumbnail() ) { ?><div class="blog__image" style="background: url('<?php echo esc_url( the_post_thumbnail_url( 'medium' ) ); ?>') 50% 50%/cover no-repeat"></div><?php } ?>
+                            <?php if ( has_post_thumbnail() ) { ?><div class="blog__image" style="background: url('<?php echo esc_url( the_post_thumbnail_url( 'medium' ) ); ?>') 50% 50%/cover no-repeat">
+                                <a class="blog__image-link" href="<?php the_permalink(); ?>"></a>
+                            </div><?php } ?>
                             <div class="blog__content-wrapper">
                                 <h3 class="blog-post__title"><a class="blog-post__title__link" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
                                     <div class="blog__categories"><?php
