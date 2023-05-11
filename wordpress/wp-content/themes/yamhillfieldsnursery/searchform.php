3e65009c2e3a6715d40a17302edeb4bc1f4f50bb
logan <logan.testa@outlook.com>
2023-05-10 22:44:52 -0700

    Improve search form positioning

--- wordpress/wp-content/themes/yamhillfieldsnursery/searchform.php
+++ wordpress/wp-content/themes/yamhillfieldsnursery/searchform.php
@@ -2,7 +2,7 @@
 
 ?>
 
-<form role="search" class="search-form" id="search-form" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+<form role="search" class="search-form" id="searchForm" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
     <div class="input-container">
         <label class="search-form__label" for=""></label>
         <input type="search" class="search-form__input" name="s" value="<?php echo esc_attr( get_search_query() ); ?>">
