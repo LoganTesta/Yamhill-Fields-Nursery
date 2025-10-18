800ec5e3a5ad5096931b8d57afa79731d5b674c6
logan <logan.testa@outlook.com>
2025-10-17 22:04:58 -0700

    Add category title to woocommerce product archive pages

--- wordpress/wp-content/themes/yamhillfieldsnursery/woocommerce/archive-product.php
+++ wordpress/wp-content/themes/yamhillfieldsnursery/woocommerce/archive-product.php
@@ -2,23 +2,31 @@
 
 /*Description: Archive pages custom template*/
 get_header(); 
+
+global $wp;
+$currentUrl = home_url( $wp->request );
+
+$productCat = basename( $currentUrl );  
 ?>
 
 <div class="inner-wrapper">
     <div class="content page-content">
-        <?php
+        <div class="content-row">
+            <div class="woocommerce-product-page-header">
+                <h2>Category: <?php echo str_replace( "-", " ", $productCat ); ?></h2>
+            </div>
+            
+            <?php
 
-        global $wp;
-        $currentUrl = home_url( $wp->request );
 
-        $productCat = basename( $currentUrl );  
-        if ( $productCat === "shop" ){
-            $productCat = "";
-        }
+            if ( $productCat === "shop" ){
+                $productCat = "";
+            }
 
-        echo do_shortcode( '[yfn_woocommerce_products_products category="' . $productCat . '"]' ); 
-        
-        ?>
+            echo do_shortcode( '[yfn_woocommerce_products_products category="' . $productCat . '"]' ); 
+
+            ?>
+        </div>
     </div>
 </div>
                              
